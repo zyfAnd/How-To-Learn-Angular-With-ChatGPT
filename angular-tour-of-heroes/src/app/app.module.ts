30b8ce643c79190948ab3500b92e78810abfab26
yanfu <18842647603@163.com>
2024-06-25 14:21:00 +0800

    component

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponentComponent } from './hello-component/hello-component.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponentComponent
+    HelloComponentComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
