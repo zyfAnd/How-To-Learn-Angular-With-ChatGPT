a94f7ebfacb48f81ded6cd33d5797a3d01a2a42d
yanfu <18842647603@163.com>
2024-07-12 15:07:07 +0800

    two-way data binding

--- angular-twoway-databinding/src/app/app.component.ts
+++ angular-twoway-databinding/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SizerComponent } from './sizer/sizer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SizerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-twoway-databinding';
+
+  fontSizePx = 16;
 }
